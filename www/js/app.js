dc5a7dc516d342248c0ac55310aad7a14abf2f96
Rafal Trzeciak <spartanik@gmail.com>
2016-03-09 00:10:38 +0100

    Desktop browser broken scroll fix, added refresher, done some cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -5,46 +5,38 @@
 // the 2nd parameter is an array of 'requires'
 var app = angular.module('holidayextras', ['ionic','ngSanitize'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform,$rootScope) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-     // cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-//      cordova.plugins.Keyboard.disableScroll(true);
-    }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 });
 
+app.config(function($ionicConfigProvider){
+  $ionicConfigProvider.scrolling.jsScrolling(false);
+})
+
 app.controller('flickr', function($scope,$http,photoData) {
 	$scope.photoData=photoData;
 	
-	$("#scriptspace").html('<script src="https://api.flickr.com/services/feeds/photos_public.gne?format=json"></script>');
-	
-    $scope.toLinkedin = function(){
+	$scope.toLinkedin = function(){
     	window.location.href= "https://www.linkedin.com/in/spartanik";
     }
     
+    $scope.doRefresh=function(){
+    	$("#scriptspace").html('<script src="https://api.flickr.com/services/feeds/photos_public.gne?format=json"></script>');
+    	$scope.$broadcast('scroll.refreshComplete');
+    }
+    
     $scope.jsonFlickrFeed = function(data){
-    	flickerData = data;  
-//    	alert(data);
-
     	$scope.photoData=data;
-//    	photoData=data;
-    	console.log(flickerData)
     	$scope.$apply();
     };
     jsonFlickrFeed = $scope.jsonFlickrFeed;
-    
+    $scope.doRefresh();
 });
 
 app.value('photoData',{});
-var flickerData;
 jsonFlickrFeed = function(){};
